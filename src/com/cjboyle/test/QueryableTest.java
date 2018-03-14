8573eceb64ff55f823ae7e87bf5dcfdc759b8348
cjboyle <connorboyle@sasktel.net>
2018-03-14 17:46:33 -0600

    Add unit test skeleton

---
+++ src/com/cjboyle/test/QueryableTest.java
@@ -0,0 +1,134 @@
+package com.cjboyle.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueryableTest {
+
+    @org.junit.jupiter.api.Test
+    void any() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void anyPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void count() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void countPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distinct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distinctPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void except() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void first() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void firstPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void firstOrDefault() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void firstOrDefaultPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void intersect() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void last() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lastPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lastOrDefault() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void lastOrDefaultPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void orderBy() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverse() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void select() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectMany() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void single() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void singlePredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void singleOrDefault() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void singleOrDefaultPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void take() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void union() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void where() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iterator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toArrayPredicate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toList() {
+    }
+}
